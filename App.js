102f1274ffb0855acdec29084283123f60ec2d36
rasyadmar <rasyadmar@gmail.com>
2023-06-06 12:11:43 +0700

    snack app.js update test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>what the fuck do you want</Text>
       <StatusBar style="auto" />
     </View>
   );
