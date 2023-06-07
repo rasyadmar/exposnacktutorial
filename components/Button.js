9dd8e1238f63fdc47902b7900a5097194554542b
rasyadmar <rasyadmar@gmail.com>
2023-06-07 22:46:37 +0700

    bugfix: use this button not work

--- components/Button.js
+++ components/Button.js
@@ -31,7 +31,7 @@ export default function Button({ label, theme, onPress }) {
     <View style={styles.buttonContainer}>
       <Pressable
         style={styles.button}
-        onPress={() => onPress={onPress}}
+        onPress={onPress}
       >
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
